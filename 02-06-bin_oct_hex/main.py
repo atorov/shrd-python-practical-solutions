acaeb576f62ea1e3a452439ce3d78124fea6b712
vpetrov-qb <vpetrov@quickbase.com>
2025-08-17 11:27:07 +0300

    02-06 bin, oct, hex

---
+++ 02-06-bin_oct_hex/main.py
@@ -0,0 +1,33 @@
+x = 5555
+
+print(bin(x))
+print(format(x, "b"))
+
+print(oct(x))
+print(format(x, "o"))
+
+print(hex(x))
+print(format(x, "x"))
+
+y = -5555
+print(bin(y))
+print(format(y, "b"))
+
+print(oct(y))
+print(format(y, "o"))
+
+print(hex(y))
+print(format(y, "x"))
+
+z = -7777
+print(bin(2**32 + z))
+print(format(2**32 + z, "b"))
+
+print(oct(2**32 + z))
+print(format(2**32 + z, "o"))
+
+print(hex(2**32 + z))
+print(format(2**32 + z, "x"))
+
+print(int("4d2", 16))
+print(int("100101110101", 2))
