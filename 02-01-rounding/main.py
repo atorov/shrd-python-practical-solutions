ee83ab3fd7c7387dce9e38c683723d4ff00b695c
vpetrov-qb <vpetrov@quickbase.com>
2025-08-17 10:29:10 +0300

    02.01 rounding (#1)

---
+++ 02-01-rounding/main.py
@@ -0,0 +1,38 @@
+n1 = round(1.24, 1)
+print(n1)
+
+n2 = round(1.28, 10)
+print(n2)
+
+n3 = round(-1.29, 1)
+print(n3)
+
+n4 = round(1.25371, 3)
+print(n4)
+
+a = 2625531
+n5 = round(a, -1)
+print(n5)
+
+n6 = round(a, -2)
+print(n6)
+
+n7 = round(a, -3)
+print(n7)
+
+x = 1.234567
+n8 = format(x, "0.2f")
+print(n8)
+
+n9 = format(x, "0.3f")
+print(n9)
+
+print("number - {:0.3f}".format(x))
+
+b = 3.1
+c = 4.2
+d = b + c
+print(d)
+
+n10 = round(d, 2)
+print(n10)
