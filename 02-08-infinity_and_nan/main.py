64c14060c6a3bbeac92dbcf2fe29ef53026dd7a1
vpetrov-qb <vpetrov@quickbase.com>
2025-08-18 15:58:46 +0300

    Infinity and NaN

---
+++ 02-08-infinity_and_nan/main.py
@@ -0,0 +1,17 @@
+import math
+
+
+a = float("inf")
+print(a)
+print(type(a))
+print(math.isinf(a))
+
+b = float("-inf")
+print(b)
+print(type(b))
+print(math.isinf(b))
+
+c = float("nan")
+print(c)
+print(type(c))
+print(math.isnan(c))
